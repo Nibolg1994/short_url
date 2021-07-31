003b277a7050990ac28264a581351848ab6fd5eb
Nibolg1994 <globin.anvi@gmail.com>
2021-07-31 17:15:42 +0300

    wrong script

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println("This wonderful world")
+	fmt.Println("Long long greeting line")
+	log.Printf("%d %d", 1, 2, 3)
+}
